7f605ce334e22ebce7119cb43269a8d8150de8a5
Dmitriy Angve <kirirch129@gmail.com>
2023-01-20 13:56:39 +0300

    #

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ window.addEventListener("DOMContentLoaded", (e) => {
       return rep.json();
     })
     .then((json) => {
-      console.log(json);
+      //   console.log(json);
       // Выводим кнопки на экран в зависимости от приходящих свойств с АПИ
       h1.innerHTML = "";
       for (const prop in json) {
@@ -49,4 +49,37 @@ function getJSON(url) {
 
 function buildPage(data) {
   console.log(data);
+  output.innerHTML = "";
+  data.results.forEach((el) => {
+    const div = document.createElement("div");
+    div.textContent = el.name || el.title;
+    div.classList.add("box");
+    div.urlz = el.url;
+    div.addEventListener("click", showItem);
+    output.append(div);
+    console.log(el.name);
+  });
+}
+
+function showItem(e) {
+  const el = e.target;
+  console.log(el.urlz);
+  output.innerHTML = "";
+  fetch(el.urlz)
+    .then((rep) => rep.json())
+    .then((data) => {
+      //   console.log(data);
+      for (const prop in data) {
+        console.log(`${prop}: ${data[prop]}`);
+        let html =
+          typeof data[prop] == "string"
+            ? data[prop]
+            : JSON.stringify(data[prop]);
+        output.innerHTML += `<div class="information">${prop} : ${html}</div>`;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      output.innerHTML = "ERROR";
+    });
 }
