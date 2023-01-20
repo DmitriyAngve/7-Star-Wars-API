f3eef72e799c5af51a919664681bc20911808ce5
Dmitriy Angve <kirirch129@gmail.com>
2023-01-20 12:47:21 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".btn");
+const h1 = document.querySelector("h1");
+const output = document.querySelector(".output");
+const inputVal = document.querySelector(".val");
+const mainURL = "https://swapi.dev/api";
+
+btn.addEventListener("click", (e) => {
+  console.log("ready");
+});
